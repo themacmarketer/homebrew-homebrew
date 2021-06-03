bc6f02d8bc3f36f4dd45e9f202553fb55cc57ee2
everydaydatascience <themacmarketer@users.noreply.github.com>
2021-06-03 23:48:36 +0800

    Update chntpw.rb

--- Formula/chntpw.rb
+++ Formula/chntpw.rb
@@ -5,7 +5,7 @@ class Chntpw < Formula
   sha256 "e915f5addc2673317285c6f022c94da7fdee415d9800cd38540a13706706786b"
   head "https://github.com/sidneys/chntpw.git"
 
-  depends_on "sidneys/homebrew/openssl@1.0"
+  depends_on "themacmarketer/homebrew/openssl@1.0"
 
   def install
     if Hardware::CPU.arm?
